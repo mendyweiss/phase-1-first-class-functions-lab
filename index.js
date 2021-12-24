ee30e9fe373834705339a092300d239d5b8791f2
Mendy <mendyw94@gmail.com>
2021-12-23 21:14:29 -0500

    just for reference sake

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (Array) => {
+    const newArray =[Array[0], Array[1]];
+    return newArray;
+};
+const returnLastTwoDrivers = (Array) => {
+    const newArray = [(Array.legnth -1), (Array.legnth -2)];
+    return newArray;
+};
