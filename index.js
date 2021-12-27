52d17abf608e7553f6a2948b074916ff00ce4484
Mendy <mendyw94@gmail.com>
2021-12-27 07:19:06 -0500

    commit tree dabdaa110e34a3a44c734abf4827bd04baf97cdc

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (Array) => {
-    const newArray =[Array[0], Array[1]];
+const returnFirstTwoDrivers = (driversNames) => {
+    const newArray =[driversNames[0], driversNames[1]];
     return newArray;
 };
-const returnLastTwoDrivers = (Array) => {
-    const newArray = [(Array.legnth -1), (Array.legnth -2)];
+const returnLastTwoDrivers = (driversNames) => {
+    //console.log(driversNames.slice(-1))
+    const newArray = driversNames.slice(-2);
+    
     return newArray;
+};
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+console.log(selectingDrivers);
+
+function createFareMultiplier (multiplier) {
+    return (originalFare) => multiplier * originalFare;
+}
+
+const fareDoubler = (createFareMultiplier(2));
+
+const fareTripler = (createFareMultiplier(3));
+
+const selectDifferentDrivers = (arrayOfDrivers, driversToBeSelected) => {
+    return driversToBeSelected(arrayOfDrivers);
 };
